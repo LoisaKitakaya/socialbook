f17b9d30a792d508cf267271270fb890eecb49d8
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-27 07:20:27 +0300

    created app database models and registered them to the admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'first_name', 'second_name', 'joined_on')
+    list_filter = ('user', 'joined_on')
+    search_fields = ['user', 'first_name', 'second_name']
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+class PostsAdmin(admin.ModelAdmin):
+
+    list_display = ('post_author', 'title', 'posted_on')
+    list_filter = ('post_author', 'posted_on')
+    search_fields = ['post_author', 'title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Posts, PostsAdmin)
+
+class CommentsAdmin(admin.ModelAdmin):
+
+    list_display = ('comment_author', 'comment_post', 'commented_on')
+    list_filter = ('comment_author', 'comment_post', 'commented_on')
+    search_fields = ['comment_author', 'comment_post', 'comment']
+
+admin.site.register(Comments, CommentsAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from slugger import AutoSlugField
 
 # Create your models here.
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='users', primary_key=True, blank=True)
+    first_name = models.CharField(max_length=50, unique=True)
+    second_name = models.CharField(max_length=50, unique=True)
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField()
+    joined_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ['-joined_on']
+
+    def __str__(self):
+
+        return self.first_name
+
+class Posts(models.Model):
+
+    post_author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_profiles', blank=True)
+    title = models.CharField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='title')
+    post_thumbnail = CloudinaryField('image')
+    content = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ['-posted_on']
+
+    def __str__(self):
+
+        return self.title
+
+class Comments(models.Model):
+
+    comment_author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, blank=True,related_name='commenting_user')
+    comment_post = models.ForeignKey(Posts, on_delete=models.CASCADE, blank=True,related_name='commented_post')
+    comment = models.TextField()
+    commented_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ['-commented_on']
+
+    def __str__(self):
+
+        return self.comment
